891d7d81e372030b5d46458cac8ed8eafbda5d06
shawon <shawonislam121009@gmail.com>
2021-08-29 18:16:59 +0600

    omitted the logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import logo from '../../images/logo.png'
 import './Header.css'
 import { Link } from 'react-router-dom'
 const Header = () => {
+    const token = JSON.parse(window.localStorage.getItem('token'))
+
+    const component = token ? <Link to="/logout">Logout</Link> : <Link to="/login">Login</Link>
     return (
         <div className="header">
-            <img src={logo} alt="" />
             <nav>
                 <Link to="/shop">Shop</Link>
                 <Link to="/review">Review Cart</Link>
-                <Link to="/login">Login</Link>
+                {component}
                 <Link to="/addproduct">Add Product</Link>
             </nav>
         </div>
